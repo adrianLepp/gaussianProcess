1f86f6f158fc396d511f21f7e78a407cfd2da14d
adrianLepp <98398878+adrianLepp@users.noreply.github.com>
2023-02-07 18:05:14 +0100

    arigoAppDev: fix threeTankKernel

--- lib/covFct/seKernelThreeTank3.m
+++ lib/covFct/seKernelThreeTank3.m
@@ -3,11 +3,11 @@ function k = seKernelThreeTank3(x1,x2,hyperparameter)
 %   sigmaF l    Hyperparameter
 %   x1 x2       Inputs (vectors allowed)
     dx13 = x1(1) - x1(3);
-    dx32 = x1(3) - x1(3);
+    dx32 = x1(3) - x1(2);
     x1 = [dx13,dx32];
     
     dx13 = x2(1) - x2(3);
-    dx32 = x2(3) - x2(3);
+    dx32 = x2(3) - x2(2);
     x2 = [dx13,dx32];
 
     k = hyperparameter.sigmaF^2 * exp(-  (x1-x2)*(1/(2*hyperparameter.l^2))*(x1-x2).');
